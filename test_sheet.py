c038e6c5134b0feab8728142fe2c9594ee78075e
sanghyunpark34-spec <sanghyun.park34@gmail.com>
2026-06-10 10:56:29 +0900

    Update test_sheet.py

--- test_sheet.py
+++ test_sheet.py
@@ -1,16 +1,25 @@
 import gspread
+import json
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 
-# 1. 시트 연결 설정
+# 1. 인증 정보 로드
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-# 'credentials.json'은 다운로드하신 파일명을 그대로 사용하세요
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
+# 환경 변수(깃허브 시크릿)에 GOOGLE_SHEETS_CREDENTIALS가 있다면 그것을 사용
+if "GOOGLE_SHEETS_CREDENTIALS" in os.environ:
+    creds_dict = json.loads(os.environ["GOOGLE_SHEETS_CREDENTIALS"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+else:
+    # 로컬 테스트용: 파일이 존재할 경우
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+
 client = gspread.authorize(creds)
 
-# 2. 시트 열기 (이름 확인)
+# 2. 시트 열기
 sheet = client.open("News_Management_DB").sheet1
 
-# 3. 테스트: 시트 구조 만들기 (첫 번째 행에 헤더 추가)
+# 3. 테스트: 헤더 추가
 header = ["Date", "Title", "Link", "Media", "Base_Score", "AI_Score", "Total_Score", "Sent"]
 sheet.insert_row(header, 1)
 
